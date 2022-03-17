8a2962d766167a426af52745ac321915500ffd13
Fernando Ribeiro da Silva <fernando1996ribeirosilva@gmail.com>
2022-03-17 15:51:56 -0300

    inserindo usuário no banco

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -3,26 +3,35 @@ import { Product } from '../models/Product';
 import User from '../models/User';
 
 export const home = async (req: Request, res: Response) => {
-    let usuarios = await User.find({});
-    console.log('USUARIOS', usuarios);
+
+   let newUser = new User();
+   newUser.name = { firstName: 'Isaias', lastName: 'Soares' };
+   newUser.email = 'isaias@hotmail.com';
+   newUser.age = 35;
+   newUser.interests = ['programação', 'jogos'];
+
+   let result = await newUser.save();
+
+   console.log("NOVO USUÁRIO", result);
 
 
-    let age: number = 90;
-    let showOld: boolean = false;
 
-    if (age > 50) {
-        showOld = true;
-    }
+   let age: number = 90;
+   let showOld: boolean = false;
 
-    let list = Product.getAll();
-    let expensiveList = Product.getFromPriceAfter(12);
+   if (age > 50) {
+      showOld = true;
+   }
 
-    res.render('pages/home', {
-        name: 'Bonieky',
-        lastName: 'Lacerda',
-        showOld,
-        products: list,
-        expensives: expensiveList,
-        frasesDoDia: []
-    });
+   let list = Product.getAll();
+   let expensiveList = Product.getFromPriceAfter(12);
+
+   res.render('pages/home', {
+      name: 'Bonieky',
+      lastName: 'Lacerda',
+      showOld,
+      products: list,
+      expensives: expensiveList,
+      frasesDoDia: []
+   });
 };
